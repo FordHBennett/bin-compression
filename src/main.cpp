f11e55ab0ee84645925a365df5431147ffe2015d
Ford Hideo Bennett <ford660bennett@tamu.edu>
2023-10-15 17:37:33 -0500

    Added statistical calculations

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include <map>
 #include <algorithm>
 #include <bitset>
+#include <cmath>
+#include <string>
 
 void generateRandomBinFile(const char* filename, long long fileSize, double zeroProbability) {
     std::ofstream outFile(filename, std::ios::binary);
@@ -65,82 +67,214 @@ std::vector<char> runLengthDecode(const std::vector<std::pair<bool, int>>& input
     return runLengthDecoded;
 }
 
-int main() {
-    const char* filename = "random_binary_file.bin";
-    const char* runLengthFilename = "run_length_encoded.bin";
-    const char* runLengthDecodedFilename = "run_length_decoded.bin";
-    long long file_size = 10000000; // Adjust the file size as needed
-    double zeroProbability = 0.5; // Adjust the probability of 0s
 
-    generateRandomBinFile(filename, file_size, zeroProbability);
-    std::cout << "Random binary file generated: " << filename << std::endl;
+// Function to perform LZW encoding
+std::vector<int> lzwEncode(const std::vector<char>& input) {
+    std::map<std::string, int> dictionary;
+    std::vector<int> lzwEncoded;
 
-    // Read the binary file
-    std::ifstream inFile(filename, std::ios::binary);
-    if (!inFile) {
+    // Initialize the dictionary with single-character entries
+    for (int i = 0; i < 256; ++i) {
+        dictionary[std::string(1, static_cast<char>(i))] = i;
+    }
+
+    std::string current = "";
+    for (char c : input) {
+        std::string next = current + c;
+        if (dictionary.find(next) != dictionary.end()) {
+            // If the current + c is in the dictionary, update the current string
+            current = next;
+        } else {
+            // Output the code for the current string and add next to the dictionary
+            lzwEncoded.push_back(dictionary[current]);
+            dictionary[next] = dictionary.size();
+            current = std::string(1, c);
+        }
+    }
+
+    if (!current.empty()) {
+        // Output the code for the remaining current string
+        lzwEncoded.push_back(dictionary[current]);
+    }
+
+    return lzwEncoded;
+}
+
+// Function to perform LZW decoding
+std::vector<char> lzwDecode(const std::vector<int>& input) {
+    std::map<int, std::string> dictionary;
+    std::vector<char> lzwDecoded;
+
+    // Initialize the dictionary with single-character entries
+    for (int i = 0; i < 256; ++i) {
+        dictionary[i] = std::string(1, static_cast<char>(i));
+    }
+
+    std::string current = dictionary[input[0]];
+    lzwDecoded.insert(lzwDecoded.end(), current.begin(), current.end());
+
+    for (size_t i = 1; i < input.size(); ++i) {
+        int code = input[i];
+        std::string entry;
+
+        if (dictionary.find(code) != dictionary.end()) {
+            entry = dictionary[code];
+        } else if (code == dictionary.size()) {
+            entry = current + current[0];
+        } else {
+            throw std::runtime_error("LZW decoding error: Invalid code.");
+        }
+
+        lzwDecoded.insert(lzwDecoded.end(), entry.begin(), entry.end());
+
+        // Add a new entry to the dictionary
+        dictionary[dictionary.size()] = current + entry[0];
+
+        current = entry;
+    }
+
+    return lzwDecoded;
+}
+
+double getFileSize(const char* filename)
+{
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
         std::cerr << "Error: Unable to open the file for reading." << std::endl;
         return 1;
     }
 
-    inFile.seekg(0, std::ios::end);
-    size_t fileSize = inFile.tellg();
-    inFile.seekg(0, std::ios::beg);
+    // Seek to the end to get the file size
+    file.seekg(0, std::ios::end);
+    double fileSize = file.tellg();
 
-    std::vector<char> binaryData(fileSize);
-    inFile.read(binaryData.data(), fileSize);
-    inFile.close();
+    // Close the file
+    file.close();
 
-    // Perform run-length encoding
-    auto startRunLength = std::chrono::high_resolution_clock::now();
-    std::vector<std::pair<bool, int>> runLengthEncoded = runLengthEncode(binaryData);
-    auto stopRunLength = std::chrono::high_resolution_clock::now();
-    auto durationRunLength = std::chrono::duration_cast<std::chrono::milliseconds>(stopRunLength - startRunLength);
-
-    // Perform run-length decoding
-    std::vector<char> runLengthDecoded = runLengthDecode(runLengthEncoded);
-
-    // Calculate sizes in bits and bytes
-    size_t runLengthSizeBits = runLengthEncoded.size();
-    size_t runLengthDecodedSizeBits = runLengthDecoded.size();
-
-    size_t runLengthSizeBytes = runLengthSizeBits / 8; // Divide by 8 to get bytes
-    size_t runLengthDecodedSizeBytes = runLengthDecodedSizeBits / 8; // Divide by 8 to get bytes
-
-    // Calculate percent differences
-    double runLengthPercentDiff = (static_cast<double>(runLengthSizeBits) - static_cast<double>(fileSize * 8)) / static_cast<double>(fileSize * 8) * 100.0;
-    double runLengthDecodedPercentDiff = (static_cast<double>(runLengthDecodedSizeBits) - static_cast<double>(fileSize * 8)) / static_cast<double>(fileSize * 8) * 100.0;
-
-    // Output statistics
-    std::cout << "Original File Size: " << fileSize << " bytes" << std::endl;
-    std::cout << "Run-Length Encoded File Size: " << runLengthSizeBytes << " bytes" << std::endl;
-    std::cout << "Run-Length Decoded File Size: " << runLengthDecodedSizeBytes << " bytes" << std::endl;
-    std::cout << "Run-Length Encoding Time: " << durationRunLength.count() << " ms" << std::endl;
-    std::cout << "Percent Difference (Run-Length Encoding): " << runLengthPercentDiff << "%" << std::endl;
-    std::cout << "Percent Difference (Run-Length Decoding): " << runLengthDecodedPercentDiff << "%" << std::endl;
-
-    // Verify that no data is lost by comparing decoded data with the original data
-    bool dataMatches = binaryData == runLengthDecoded;
-    std::cout << "Data Matches: " << (dataMatches ? "Yes" : "No") << std::endl;
-
-    // Create a binary file from run-length encoded data for further verification
-    std::ofstream runLengthOutFile(runLengthFilename, std::ios::binary);
-    std::ofstream runLengthDecodedOutFile(runLengthDecodedFilename, std::ios::binary);
-    if (!runLengthOutFile || !runLengthDecodedOutFile) {
-        std::cerr << "Error: Unable to create the run-length encoded or decoded file." << std::endl;
+    if (fileSize < 0) {
+        std::cerr << "Error: Unable to determine the file size." << std::endl;
         return 1;
     }
+    return fileSize;
+}
+int main() {
+    const int numIterations = 10;
+    const char* filename = "random_binary_file.bin";
+    const char* runLengthFilename = "run_length_encoded.bin";
+    const char* runLengthDecodedFilename = "run_length_decoded.bin";
+    const char* lzwEncodedFilename = "lzw_encoded.bin";
+    const char* lzwDecodedFilename = "lzw_decoded.bin";
+    long long file_size = 10000000; // Adjust the file size as needed
+    double zeroProbability = 0.5; // Adjust the probability of 0s
 
-    for (size_t i = 0; i < runLengthEncoded.size(); ++i) {
-        char byte = static_cast<char>(runLengthEncoded[i].first);
-        runLengthOutFile.write(reinterpret_cast<const char*>(&byte), sizeof(char));
+    double avgRunLengthSizeBytes = 0;
+    double avgRunLengthTimeMs = 0;
+    double avgLzwSizeBytes = 0;
+    double avgLzwTimeMs = 0;
+
+    for (int i = 0; i < numIterations; ++i) {
+
+        generateRandomBinFile(filename, file_size, zeroProbability);
+
+        // Read the binary file
+        std::ifstream inFile(filename, std::ios::binary);
+        if (!inFile) {
+            std::cerr << "Error: Unable to open the file for reading." << std::endl;
+            return 1;
+        }
+
+        inFile.seekg(0, std::ios::end);
+        size_t fileSize = inFile.tellg();
+        inFile.seekg(0, std::ios::beg);
+
+        std::vector<char> binaryData(fileSize);
+        inFile.read(binaryData.data(), fileSize);
+        inFile.close();
+
+        // Perform run-length encoding
+        auto startRunLength = std::chrono::high_resolution_clock::now();
+        std::vector<std::pair<bool, int>> runLengthEncoded = runLengthEncode(binaryData);
+        auto stopRunLength = std::chrono::high_resolution_clock::now();
+        auto durationRunLength = std::chrono::duration_cast<std::chrono::milliseconds>(stopRunLength - startRunLength);
+
+        std::vector<char> runLengthDecoded = runLengthDecode(runLengthEncoded);
+        bool dataMatches = binaryData == runLengthDecoded;
+        std::cout << "Data Matches: " << (dataMatches ? "Yes" : "No") << std::endl;
+
+        // Write the run-length encoded and decoded files
+        std::ofstream runLengthOutFile(runLengthFilename, std::ios::binary);
+        std::ofstream runLengthDecodedOutFile(runLengthDecodedFilename, std::ios::binary);
+        if (!runLengthOutFile || !runLengthDecodedOutFile) {
+            std::cerr << "Error: Unable to create the run-length encoded or decoded file." << std::endl;
+            return 1;
+        }
+
+        for (size_t i = 0; i < runLengthEncoded.size(); ++i) {
+            char byte = static_cast<char>(runLengthEncoded[i].first);
+            runLengthOutFile.write(reinterpret_cast<const char*>(&byte), sizeof(char));
+        }
+
+        for (size_t i = 0; i < runLengthDecoded.size(); ++i) {
+            char byte = runLengthDecoded[i];
+            runLengthDecodedOutFile.write(reinterpret_cast<const char*>(&byte), sizeof(char));
+        }
+
+        runLengthOutFile.close();
+        runLengthDecodedOutFile.close();
+
+        // open the run-length encoded file and determine the file size
+        avgRunLengthSizeBytes += getFileSize(runLengthFilename);
+        avgRunLengthTimeMs += durationRunLength.count();
+
+        // Perform LZW encoding
+        auto startLzw = std::chrono::high_resolution_clock::now();
+        std::vector<int> lzwEncoded = lzwEncode(binaryData);
+        auto stopLzw = std::chrono::high_resolution_clock::now();
+        auto durationLzw = std::chrono::duration_cast<std::chrono::milliseconds>(stopLzw - startLzw);
+
+        // Perform LZW decoding
+        std::vector<char> lzwDecoded = lzwDecode(lzwEncoded);
+
+        // Verify that no data is lost by comparing decoded data with the original data
+        bool lzwDataMatches = binaryData == lzwDecoded;
+        std::cout << "LZW Data Matches: " << (lzwDataMatches ? "Yes" : "No") << std::endl;
+
+        // Create a binary file from LZW encoded data for further verification
+        std::ofstream lzwOutFile(lzwEncodedFilename, std::ios::binary);
+        std::ofstream lzwDecodedOutFile(lzwDecodedFilename, std::ios::binary);
+        if (!lzwOutFile || !lzwDecodedOutFile) {
+            std::cerr << "Error: Unable to create the LZW encoded or decoded file." << std::endl;
+            return 1;
+        }
+
+        for (size_t i = 0; i < lzwEncoded.size(); ++i) {
+            int code = lzwEncoded[i];
+            lzwOutFile.write(reinterpret_cast<const char*>(&code), sizeof(int));
+        }
+
+        for (size_t i = 0; i < lzwDecoded.size(); ++i) {
+            char byte = lzwDecoded[i];
+            lzwDecodedOutFile.write(reinterpret_cast<const char*>(&byte), sizeof(char));
+        }
+
+        lzwOutFile.close();
+        lzwDecodedOutFile.close();
+
+        // open the LZW encoded file and determine the file size
+        avgLzwSizeBytes += getFileSize(lzwEncodedFilename);
+
+        avgLzwTimeMs += durationLzw.count();
     }
 
-    for (size_t i = 0; i < runLengthDecoded.size(); ++i) {
-        char byte = runLengthDecoded[i];
-        runLengthDecodedOutFile.write(reinterpret_cast<const char*>(&byte), sizeof(char));
-    }
+    avgRunLengthSizeBytes /= numIterations;
+    avgRunLengthTimeMs /= numIterations;
+    avgLzwSizeBytes /= numIterations;
+    avgLzwTimeMs /= numIterations;
 
-    runLengthOutFile.close();
-    runLengthDecodedOutFile.close();
+    std::cout << "Average Run-Length Encoded File Size: " << avgRunLengthSizeBytes << " bytes" << std::endl;
+    std::cout << "Average Run-Length Encoding Time: " << avgRunLengthTimeMs << " ms" << std::endl;
+    std::cout << "Average LZW Encoded File Size: " << avgLzwSizeBytes << " bytes" << std::endl;
+    std::cout << "Average LZW Encoding Time: " << avgLzwTimeMs << " ms" << std::endl;
+
     return 0;
 }
